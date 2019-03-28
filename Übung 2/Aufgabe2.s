2993eed7cd2c8f5b229118471c4ee94f5ce1ebe8
Shynixn <shynixn@gmail.com>
2019-03-28 16:56:21 +0100

    Finished Aufgabe2.

--- Übung 2/Aufgabe2.s
+++ Übung 2/Aufgabe2.s
@@ -18,42 +18,69 @@ main:
     sw   $ra, 4($sp)   # Preserve return address.
     sw   $fp, 0($sp)   # Preserve frame pointer.
 
-    addi $a0, $0, 5    # Sets the first argument to 5.
+    addi $a0, $0, 10   # Sets the first argument to (10). <-
     jal catalan        # Call the catalan function.
 
-    lw   $fp, 0($sp)   # restore frame pointer
-   	lw   $ra, 4($sp)   # restore return address
-   	jr $ra
+    add $s0, $0, $v0  # Result (16796) is stored in $s0.  <-
+
+    lw   $fp, 0($sp)   # Restore frame pointer
+   	lw   $ra, 4($sp)   # Restore return address
+   	jr $ra             # Exit.
 
 catalan:
-    addi $sp, $sp, -12 # Reserve space for 3 words (return address, argument n, frame pointer).
+    addi $sp, $sp, -24 # Reserve space for 3 words (return address, argument n, frame pointer).
     sw   $ra, 8($sp)   # Preserve return address.
-    sw   $a0, 4($sp)   # Preserve argument n.
+    sw   $s0, 4($sp)   # Store the callee safe register $s0 on the stack.
     sw   $fp, 0($sp)   # Preserve frame pointer.
-    addi $fp, $sp, 12  # Adjust frame pointer.
+    addi $fp, $sp, 24  # Adjust frame pointer.
 
-    addi $t3, $0, 1    # Cache value 1 in register t3 for comparison.
-    addi $s0, $0, 1    # Set $s0 to 1.
+    addi $t1, $0, 1    # Cache value 1 in register $t1 for comparison.
+    addi $v0, $0, 1    # Set the return value of the function to 1.
 
-    slt $t1, $a0, 2                 # Store result of (n<2) in t1.
-    beq $t3, $t1, catalan_return    # Immediately return 1 if n is smaller than 2. 1 == t1
+    slt $t2, $a0, 2    # Store result of n smaller 2 in $t2. Immediately return 1 if n is smaller than 2.
+    beq $t1, $t2, catalan_return
 
     addi $s0, $0, 0    # Set $s0 (res) to 0.
     addi $t0, $0, 0    # Set $t0 (i) to 0.
-    add $t2, $0, $a0    # Cache argument $a (n) in $t2.
-catalan_loop:
-    slt $t1, $t0, $t2               # Check if $0(i) < $t2(n).
-    beq $0, $t1, catalan_return     # If not smaller then go to return.
+    add  $t3, $0, $a0  # Cache argument $a0 (n) in $t3.
 
-    add $a0, $0, $t0  # Sets the first argument to i. $t0.
+catalan_loop:
+    add $v0, $0, $s0                # Cache $s0 (res) as return value.
+    slt $t2, $t0, $t3               # Check if $0(i) < $t2(n).
+    beq $0, $t2, catalan_return     # If not smaller then go to return.
+
+    sw $t0, 20($sp)     # Store temp register $t0(i) on the stack. (The caller is responsible)
+    sw $t3, 16($sp)     # Store temp register $t3(n) on the stack.  (The caller is responsible)
+
+    add $a0, $0, $t0   # Sets the first argument to $t0(i).
     jal  catalan       # catalan(i)
+
+    lw $t3, 16($sp)   # Restore temp register $t3(n) from the stack.
+    lw $t0, 20($sp)   # Restore temp register $t0(i) from the stack.
+
+    addi $a0, $0, -1  # Set the parameter of catalan(n-i-1).
+    add $a0, $a0, $t3
+    sub $a0, $a0, $t0
+
+    sw $t0, 20($sp)     # Store temp register $t0(i) on the stack. (The caller is responsible)
+    sw $t3, 16($sp)     # Store temp register $t3(n) on the stack. (The caller is responsible)
+    sw $v0, 12($sp)     # Store temp register catalan(i) result on the stack. (The caller is responsible)
+
+    jal  catalan      # catalan(n-i-1)
+
+    lw $t4, 12($sp)   # Restore temp register catalan(i) result from the stack.
+    lw $t3, 16($sp)   # Restore temp register $t3(n) from the stack.
+    lw $t0, 20($sp)   # Restore temp register $t0(i) from the stack.
+
+    mul $t4, $t4, $v0  # Multiply catalan(i)*catalan(n-i-1) and store result in $t4.
+    add $s0, $s0, $t4 # Add the result. res += catalan(i)*catalan(n-i-1);
 
     addi $t0, $t0, 1 # Perform i++.
     j catalan_loop   # Jump to loop begin.
 
-
 catalan_return:
     lw   $fp, 0($sp)   # Restore frame pointer.
-    lw   $ra, 8($sp)   # restore return address.
-    addi $sp, $sp, 12  # clean up stack.
+    lw   $s0, 4($sp)   # Restore the callee safe register $s0 from the stack.
+    lw   $ra, 8($sp)   # Restore return address.
+    addi $sp, $sp, 24  # clean up stack.
     jr   $ra	       # Use return address to jump back.
